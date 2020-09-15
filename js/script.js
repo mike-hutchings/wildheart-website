482733878c51b69958af2558b6b3bf5d94b9dd34
Mike Hutchings <mike.hutchings@protonmail.com>
2020-09-15 18:56:32 -0230

    Colour updates, new pages, general cleanup

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-const class_times = document.querySelector("#class-times").childNodes;
+const class_times1 = document.querySelector("#class-times1").childNodes;
+const class_times2 = document.querySelector("#class-times2").childNodes;
 
-class_times.forEach(class_time => {
-  if (class_time.nodeType == 1) {
-    const text = class_time.innerHTML;
-    const time = new Date("1/1/2000 " + text.substring(text.indexOf(">") + 1));
-    let time_string = time.getHours() > 12 ? time.getHours() - 12: time.getHours();
-    time_string += ":";
-    time_string += time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-    time_string += time.getHours() > 12 ? " pm": " am";
+const schedule_rows = [class_times1, class_times2];
 
-    class_time.innerHTML = `${text.substring(0, text.indexOf(">") + 1)} ${time_string}`;
-  }
+schedule_rows.forEach(class_times => {
+  class_times.forEach(class_time => {
+    if (class_time.nodeType == 1) {
+      const text = class_time.innerHTML;
+      if (text != "") {
+        const time = new Date("1/1/2000 " + text.substring(text.indexOf(">") + 1));
+        let time_string = time.getHours() > 12 ? time.getHours() - 12: time.getHours();
+        time_string += ":";
+        time_string += time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+        time_string += time.getHours() > 12 ? " pm": " am";
+  
+        class_time.innerHTML = `${text.substring(0, text.indexOf(">") + 1)} ${time_string}`;
+      }
+    }
+  });
 });
